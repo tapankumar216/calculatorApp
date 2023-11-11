d12c12f186c2857f045940a68930c643ee48290b
Tapan Kumar Mahanta <mtapankumar216@gmail.com>
2023-11-11 16:48:10 +0530

    Third Update

--- app/src/main/java/com/example/calculatorapp/splashScreen.kt
+++ app/src/main/java/com/example/calculatorapp/splashScreen.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
-class splashScreen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
